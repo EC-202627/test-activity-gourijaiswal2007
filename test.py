aec4357e6a2f192d468bd4886dde949f8e003395
gourijaiswal2007 <gouri.jaiswal@avantika.edu.in>
2026-03-27 13:19:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-X ml of water today. Determine if the client followed the doctor's advice or not.
-Input Format
+t = int(input())
 
-The first line contains a single integer 
-T — the number of test cases. Then the test cases follow.
-The first and only line of each test case contains one integer 
-X — the amount of water client drank today.
+for _ in range(t):
+    x = int(input())
+
+    if x <= 1000:
+        print("No")
+    elif x >= 10000:
+        print("It's too much")
+    else:
+        print("Yes")
